bddc0dd44d45cb1f04d47fece4305623369e59ee
Julian Feliciano <jfelic@Julians-MacBook-Pro.local>
2024-02-08 15:25:33 -0500

    Changed how we are creating arrays and how we are operating on them
    Moved away from creating array with constructor and private attributes to simplify things.

--- src/App.java
+++ src/App.java
@@ -1,29 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Sorting arr = new Sorting(10);
+        int[] myArray = Sorting.createArray(100);
 
-        arr.insert(77);
-        arr.insert(99);
-        arr.insert(44);
-        arr.insert(55);
-        arr.insert(22);
-        arr.insert(88);
-        arr.insert(11);
-        arr.insert(00);
-        arr.insert(66);
-        arr.insert(33);
-
-        System.out.print( "Unsorted: "); arr.display(); System.out.println();
+        System.out.print( "Unsorted:\n"); 
+        Sorting.display(myArray);
         
-        arr.insertionSort();
-        System.out.print("\nSorted: "); arr.display(); System.out.println();
+        Sorting.bubbleSortPractice(myArray);
+        System.out.print("\n\nSorted:\n"); 
+        Sorting.display(myArray);
+        
 
-        Testing.testingTimeComplexity();
-        Testing.testingTimeComplexity();
-        Testing.testingTimeComplexity();
+        // Testing.testingTimeComplexity();
+        // Testing.testingTimeComplexity();
+        // Testing.testingTimeComplexity();
 
-        Testing.whatSortToWrite();
+        // Testing.whatSortToWrite();
 
     }
 }
--- src/Sorting.java
+++ src/Sorting.java
@@ -1,60 +1,20 @@
-public class Sorting {
+public class Sorting { 
+
+    public static int[] createArray(int size) {
+        int[] newArray = new int[size];
+        for(int i = 0; i < size; i++) {
+            newArray[i] = (int)(Math.random() * 10000);
+        }
+        return newArray;
+    }
+
+    public static void display(int[] arr) {
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
     
-    private int[] arr;
-    private static int nElems; // # of data items
-
-    public Sorting(int size) {
-        arr = new int[size]; // create the array
-        nElems = 0; // no elements in array yet
-    }
-
-    public void insert(int value) {
-        arr[nElems] = value; // insert
-        nElems++; // increment size to keep track of insertion index
-    }
-
-    public void display() {
-        for(int i = 0; i < arr.length; ++i) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    public void bubbleSortBook() {
-        int i, j, temp;
-        int n = arr.length;
-        
-        for(i = n - 1; i > 0; i--) { // instead of i = nElems - 1, we can use i = n - 1
-            for(j = 0; j < i; j++) {
-                if(arr[j] > arr[j + 1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-
-    public void selectionSortBook() {
-        int n = arr.length;
-        int minIndex;
-
-        for(int i = 0; i < n-1; i++) {
-            minIndex = i;
-            for(int j = i + 1; j < n; j ++) {
-                if(arr[j] < arr[minIndex]) {
-                    minIndex = j; // min index is now the index of j (the new min value)
-                }
-            }
-            if(minIndex != i) { // means we have a new min value
-                int temp = arr[i];
-                arr[i] = arr[minIndex];
-                arr[minIndex] = temp; 
-            }
-        }
-    }
-
-    public void newBubbleSort() {
+    public static void bubbleSort(int[] arr) {
         int i,j,n;
         n = arr.length;
         for(i = n - 1; i > 0; i--) {
@@ -68,23 +28,22 @@ public class Sorting {
         }
     }
 
-    public void newerBubbleSort() {
+    public static void bubbleSortPractice(int[] arr) {
         int i, j, temp;
         int n = arr.length;
 
         for(i = n - 1; i > 0; i--) {
             for(j = 0; j < i; j++) {
-                if(arr[j] > arr[j+1]) {
+                if(arr[j] > arr[j + 1]) {
                     temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
-
     }
 
-    public void newSelectionSort() {
+    public static void newSelectionSort(int[] arr) {
         int i, j, minIndex, temp;
         int n = arr.length;
 
@@ -103,7 +62,7 @@ public class Sorting {
         }
     }
 
-    public void insertionSort() {
+    public static void insertionSort(int[] arr) {
         int i, j;
         
                                             // i acts as a cutoff point in the array, separating
@@ -118,4 +77,41 @@ public class Sorting {
         arr[j] = temp;
         }
     }
+
+    // /* ## Beginning of Quick sort methods 
+    //  * We will need a partitioning method
+    //  * We will need a sorting method
+    // */
+
+    // // Partition
+    // public int Partition() {
+    //     int lowIndex, highIndex, pivot, midpoint;
+    //     boolean done = false;
+
+    //     lowIndex = arr[0];
+    //     highIndex = arr.length - 1;
+    //     midpoint = (lowIndex + highIndex) / 2;
+    //     pivot = arr[midpoint];
+
+    //     while( done != true) {
+    //         while(arr[lowIndex] < pivot) {
+    //             lowIndex += 1;
+    //         }
+    //         while(arr[highIndex] > pivot) {
+    //             highIndex -= 1;
+    //         } 
+    //     }
+
+    //     // swap
+    //     if(lowIndex >= highIndex) {
+    //         done = true;
+    //         return highIndex;
+    //     } else {
+    //         int temp = arr[lowIndex];
+    //         arr[lowIndex] = arr[highIndex];
+    //         arr[highIndex] = temp;
+    //     }
+    // }
+
+    // Implement Quick sort
 }
